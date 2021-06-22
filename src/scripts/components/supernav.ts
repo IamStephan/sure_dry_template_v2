e487b65ccd6b725dc4bde170d298443608d6fdaa
iamstephan <stephanburger54@gmail.com>
2021-06-22 20:45:11 +0200

    added supernav component

---
+++ src/scripts/components/supernav.ts
@@ -0,0 +1,22 @@
+import tippy from "tippy.js";
+
+function InitSuperNav(): void {
+  tippy(".item-with-children", {
+    content: (ref) => {
+      const childrenContainer = ref.querySelector(".children-container");
+
+      if (childrenContainer) {
+        return childrenContainer;
+      }
+
+      return null;
+    },
+    offset: [0, 18],
+    placement: "bottom",
+    allowHTML: true,
+    interactive: true,
+    duration: 50,
+  });
+}
+
+InitSuperNav();

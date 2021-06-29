5dba6e8b1fb8985648f0f32d75725319b8c957dc
iamstephan <stephanburger54@gmail.com>
2021-06-29 15:15:41 +0200

    borders update

--- src/template_borders/borders.php
+++ src/template_borders/borders.php
@@ -1,9 +1,14 @@
 <?php
 // this is here to prevent loading extra homepage content. Remove when we copy stuff into index
 global $thePage, $cmsPageData;
-if ($thePage == "index") {
+
+
+  $extraPageData = $cmsPageData["page.body_content"];
+  
+  // This gets injected into the body tag at the end
+  // Content should be in the main tag inside the body
+  // accessibility and seo
   $cmsPageData["page.body_content"] = "";
-}
 ?>
 
 <?php
@@ -24,6 +29,21 @@ $socialFooter = '
   <img src="/core/images/templates/health/yt-icon-light.png" alt="YouTube"></a>
   ';
 ?>
+
+<?php
+    // Use this var to select the page based on type
+    // I want to use switch statements but...PHP
+    $pageType = "";
+    if($thePage == "index") {
+        $pageType = 'HOME';
+    } else if(($thePage != "index") && ($thePage != "free-estimate") && ($thePage != "free-estimate/confirmation")) {
+        $pageType = 'CONTENT';
+    } else {
+        $pageType = 'UNKNOW';
+    }
+?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -48,6 +68,7 @@ $socialFooter = '
 
   <!-- Styles (Mix global and paged import in Template css)-->
   <link rel="stylesheet" type="text/css" href="/inc/sb-styles.css?custom_template_id=129">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body id="app">
@@ -68,24 +89,24 @@ $socialFooter = '
           <div class="cta">
             <span class="cta-text"> 1-800-379-3788 </span>
             <span class="cta-text-small">
-            <svg
-              class='phone'
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <path
-                d="M22 12C22 10.6868 21.7413 9.38647 21.2388 8.1731C20.7362 6.95996 19.9997 5.85742 19.0711 4.92896C18.1425 4.00024 17.0401 3.26367 15.8268 2.76123C14.6136 2.25854 13.3132 2 12 2V4C13.0506 4 14.0909 4.20703 15.0615 4.60889C16.0321 5.01099 16.914 5.60034 17.6569 6.34326C18.3997 7.08594 18.989 7.96802 19.391 8.93848C19.7931 9.90918 20 10.9495 20 12H22Z"
-                fill="white"
-              />
-              <path
-                d="M2 10V5C2 4.44775 2.44772 4 3 4H8C8.55228 4 9 4.44775 9 5V9C9 9.55225 8.55228 10 8 10H6C6 14.4182 9.58173 18 14 18V16C14 15.4478 14.4477 15 15 15H19C19.5523 15 20 15.4478 20 16V21C20 21.5522 19.5523 22 19 22H14C7.37259 22 2 16.6274 2 10Z"
-                fill="white"
-              />
-              <path
-                d="M17.5433 9.70386C17.8448 10.4319 18 11.2122 18 12H16.2C16.2 11.4485 16.0914 10.9023 15.8803 10.3928C15.6692 9.88306 15.3599 9.42017 14.9698 9.03027C14.5798 8.64014 14.1169 8.33081 13.6073 8.11963C13.0977 7.90869 12.5515 7.80005 12 7.80005V6C12.7879 6 13.5681 6.15527 14.2961 6.45679C15.024 6.7583 15.6855 7.2002 16.2426 7.75732C16.7998 8.31445 17.2418 8.97583 17.5433 9.70386Z"
-                fill="white"
-              />
-            </svg>
+                <svg
+                    class="phone"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M22 12C22 10.6868 21.7413 9.38647 21.2388 8.1731C20.7362 6.95996 19.9997 5.85742 19.0711 4.92896C18.1425 4.00024 17.0401 3.26367 15.8268 2.76123C14.6136 2.25854 13.3132 2 12 2V4C13.0506 4 14.0909 4.20703 15.0615 4.60889C16.0321 5.01099 16.914 5.60034 17.6569 6.34326C18.3997 7.08594 18.989 7.96802 19.391 8.93848C19.7931 9.90918 20 10.9495 20 12H22Z"
+                        fill="white"
+                      />
+                      <path
+                        d="M2 10V5C2 4.44775 2.44772 4 3 4H8C8.55228 4 9 4.44775 9 5V9C9 9.55225 8.55228 10 8 10H6C6 14.4182 9.58173 18 14 18V16C14 15.4478 14.4477 15 15 15H19C19.5523 15 20 15.4478 20 16V21C20 21.5522 19.5523 22 19 22H14C7.37259 22 2 16.6274 2 10Z"
+                        fill="white"
+                      />
+                      <path
+                        d="M17.5433 9.70386C17.8448 10.4319 18 11.2122 18 12H16.2C16.2 11.4485 16.0914 10.9023 15.8803 10.3928C15.6692 9.88306 15.3599 9.42017 14.9698 9.03027C14.5798 8.64014 14.1169 8.33081 13.6073 8.11963C13.0977 7.90869 12.5515 7.80005 12 7.80005V6C12.7879 6 13.5681 6.15527 14.2961 6.45679C15.024 6.7583 15.6855 7.2002 16.2426 7.75732C16.7998 8.31445 17.2418 8.97583 17.5433 9.70386Z"
+                        fill="white"
+                      />
+                </svg>
               Call Now
             </span>
           </div>
@@ -134,6 +155,10 @@ $socialFooter = '
     
 
   <main>
+      
+    <?php
+        if($pageType == "HOME") {
+    ?>
     <!-- Hero Section -->
     <section class="hero-section-layout space-section">
       <div class="hero-bg">
@@ -652,7 +677,21 @@ $socialFooter = '
         </p>
       </div>
     </section>
-
+    <?php
+        }
+    ?>
+    
+    <?php
+        if($pageType == "CONTENT") {
+    ?>
+    <!-- Content Section -->
+    <section class="content space-section">
+      <?php echo $extraPageData; ?>
+    </section>
+    <?php
+        }
+    ?>
+    
   </main>
 
   <!-- TODO: make this dynamic -->

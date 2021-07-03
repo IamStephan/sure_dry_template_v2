ceb3ad1e12b03012eca3edfad38a7b56d8864d90
iamstephan <stephanburger54@gmail.com>
2021-07-03 23:19:34 +0200

    added min properties to allow different impl.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,26 +10,22 @@ module.exports = {
 
     screens: {
       "3xl": { max: "1919px" },
-      // => @media (max-width: 1535px) { ... }
-
       "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
-
       xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-
       lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
-
       md: { max: "767px" },
-      // => @media (max-width: 767px) { ... }
-
       sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
+
+      "min-3xl": { min: "1919px" },
+      "min-2xl": { min: "1535px" },
+      'min-xl': { min: "1279px" },
+      'min-lg': { min: "1023px" },
+      'min-md': { min: "767px" },
+      'min-sm': { min: "639px" }
     },
     extend: {
       colors: {
-        primary: "#F01719",
+        primary: "#D4392A",
         secondary: "#0F71B3",
         gray: {
           50: "#f7f7f7",

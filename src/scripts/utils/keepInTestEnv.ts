4175ad6687eb80698e7f16003f2606bea6b577ed
iamstephan <stephanburger54@gmail.com>
2021-07-05 12:22:38 +0200

    keep navigation within the dev enviroment

--- src/scripts/utils/keepInTestEnv.ts
+++ src/scripts/utils/keepInTestEnv.ts
@@ -2,3 +2,20 @@
  * Override link navigations and append url params
  * to keep navigating within the test enviroment
  */
+
+function KeepInTestEnv(): void {
+  const LinkTags = document.querySelectorAll<HTMLAnchorElement>("a[href]");
+
+  LinkTags.forEach((linkElem) => {
+    linkElem.addEventListener("click", (ev) => {
+      ev.preventDefault();
+      const link = ev.target as HTMLAnchorElement;
+
+      window.location.href =
+        link.attributes.getNamedItem("href").value +
+        "?cache=0&dev_template=1&test=1";
+    });
+  });
+}
+
+KeepInTestEnv();

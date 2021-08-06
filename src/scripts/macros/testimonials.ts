53c157ad8ea0fa7da977ec2248c48dd421850392
iamstephan <stephanburger54@gmail.com>
2021-08-06 12:28:17 +0200

    testimonial macro

---
+++ src/scripts/macros/testimonials.ts
@@ -0,0 +1,22 @@
+function InitContentSelector(): void {
+  const selector =
+    document.querySelector<HTMLSelectElement>("#content_selector");
+
+  const writtenContent =
+    document.querySelector<HTMLDivElement>("#tabs-written");
+  const videoContent = document.querySelector<HTMLDivElement>("#tabs-video");
+
+  selector.addEventListener("change", (_e) => {
+    if (selector.value === "video") {
+      writtenContent.classList.add("hide");
+      videoContent.classList.remove("hide");
+    }
+
+    if (selector.value === "written") {
+      writtenContent.classList.remove("hide");
+      videoContent.classList.add("hide");
+    }
+  });
+}
+
+InitContentSelector();

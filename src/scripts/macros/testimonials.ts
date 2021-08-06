b068ab3326f21fdd8932c8ec2647b2f9a33ecfbe
iamstephan <stephanburger54@gmail.com>
2021-08-06 13:51:57 +0200

    prevent errors

--- src/scripts/macros/testimonials.ts
+++ src/scripts/macros/testimonials.ts
@@ -6,17 +6,19 @@ function InitContentSelector(): void {
     document.querySelector<HTMLDivElement>("#tabs-written");
   const videoContent = document.querySelector<HTMLDivElement>("#tabs-video");
 
-  selector.addEventListener("change", (_e) => {
-    if (selector.value === "video") {
-      writtenContent.classList.add("hide");
-      videoContent.classList.remove("hide");
-    }
+  if (selector) {
+    selector.addEventListener("change", (_e) => {
+      if (selector.value === "video") {
+        writtenContent.classList.add("hide");
+        videoContent.classList.remove("hide");
+      }
 
-    if (selector.value === "written") {
-      writtenContent.classList.remove("hide");
-      videoContent.classList.add("hide");
-    }
-  });
+      if (selector.value === "written") {
+        writtenContent.classList.remove("hide");
+        videoContent.classList.add("hide");
+      }
+    });
+  }
 }
 
 InitContentSelector();

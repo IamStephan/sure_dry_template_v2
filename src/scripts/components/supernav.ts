a1ce2024bcd6ce5b98e2e800d07f8c1e90285d84
iamstephan <stephanburger54@gmail.com>
2021-08-23 11:09:25 +0200

    supernav accounts for multiple nav and errors

--- src/scripts/components/supernav.ts
+++ src/scripts/components/supernav.ts
@@ -4,55 +4,89 @@
  * I can't use the focus-within selector since it's
  * not supported ni Edge 15
  *
+ * I'm also accounting for multiple supernavs (clones)
+ *
+ * TODO: Handle click outside
  */
 
-function InitMobileNav() {
-  const ToggleMenuButton =
-    document.querySelector<HTMLButtonElement>("#menu-button");
-  const RootNode = document.querySelector<HTMLUListElement>(".level-1.small");
-  const SubMenus = RootNode.querySelectorAll<HTMLLIElement>(".with-children");
+class MobileNav {
+  supernavs: NodeListOf<HTMLDivElement>;
 
-  function resetAll() {
-    RootNode.classList.remove("nest");
-    SubMenus.forEach((element) => {
-      element.querySelector(".level-2").classList.remove("show");
+  constructor() {
+    this._getSupernavs();
+
+    this._configureSuperNav();
+  }
+
+  private _getSupernavs() {
+    this.supernavs = document.querySelectorAll(".supernav");
+  }
+
+  private _configureSuperNav() {
+    this.supernavs.forEach((supernav) => {
+      const toggleButton =
+        supernav.querySelector<HTMLButtonElement>(".menu-button");
+      const rootNode =
+        supernav.querySelector<HTMLUListElement>(".level-1.small");
+      const subMenus =
+        supernav.querySelectorAll<HTMLLIElement>(".with-children");
+
+      toggleButton.addEventListener("click", () => {
+        if (rootNode.classList.contains("show")) {
+          this._resetAll(rootNode, subMenus);
+        }
+        rootNode.classList.toggle("show");
+      });
+
+      subMenus.forEach((subMenu) => {
+        const moveButton = subMenu.querySelector<HTMLDivElement>(".dropdown");
+        const list = subMenu.querySelector<HTMLUListElement>(".level-2");
+
+        moveButton.addEventListener("click", () =>
+          this._goDeeper(subMenus, rootNode, list)
+        );
+
+        list
+          .querySelector<HTMLButtonElement>(".return-level-1")
+          .addEventListener("click", () => this._goShallow(rootNode));
+      });
     });
   }
 
-  function resetSubMenus() {
-    SubMenus.forEach((element) => {
-      element.querySelector(".level-2").classList.remove("show");
+  private _goDeeper(
+    submenus: NodeListOf<HTMLLIElement>,
+    rootNode: HTMLUListElement,
+    list: HTMLUListElement
+  ) {
+    this._resetSubMenus(submenus);
+    rootNode.classList.add("nest");
+    list.classList.add("show");
+  }
+
+  private _goShallow(rootNode: HTMLUListElement) {
+    rootNode.classList.remove("nest");
+  }
+
+  private _resetSubMenus(subMenus: NodeListOf<HTMLLIElement>) {
+    subMenus.forEach((submenu) => {
+      submenu.querySelector(".level-2").classList.remove("show");
     });
   }
 
-  ToggleMenuButton.addEventListener("click", () => {
-    if (RootNode.classList.contains("show")) {
-      resetAll();
-    }
-    RootNode.classList.toggle("show");
-  });
+  private _resetAll(
+    rootNode: HTMLUListElement,
+    submenus: NodeListOf<HTMLLIElement>
+  ) {
+    rootNode.classList.remove("nest");
 
-  SubMenus.forEach((element) => {
-    const MoveButton = element.querySelector<HTMLDivElement>(".dropdown");
-    const List = element.querySelector<HTMLUListElement>(".level-2");
-
-    function goDeeper() {
-      resetSubMenus();
-      RootNode.classList.add("nest");
-      List.classList.add("show");
-    }
-
-    function goShallow() {
-      RootNode.classList.remove("nest");
-    }
-
-    MoveButton.addEventListener("click", goDeeper);
-
-    List.querySelector<HTMLButtonElement>(".return-level-1").addEventListener(
-      "click",
-      goShallow
-    );
-  });
+    submenus.forEach((submenu) => {
+      submenu.querySelector(".level-2").classList.remove("show");
+    });
+  }
 }
 
-InitMobileNav();
+try {
+  new MobileNav();
+} catch (e) {
+  console.error(`Could not init Mobile supernav: ${e}`);
+}
